3958dfeeb177e955d4162a6e31ce203d0f4b7153
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-30 17:39:13 +0600

    break steatment done

---
+++ loops/while loop/break.js
@@ -0,0 +1,14 @@
+
+for(let i=0; i<=10;i++){
+    if(i === 5){
+        break
+    }
+    document.write(i)
+} 
+
+for(let i=5;i<=50;i++){
+    if(i===30){
+        break
+    }
+    document.write(i,'<br>')
+}
