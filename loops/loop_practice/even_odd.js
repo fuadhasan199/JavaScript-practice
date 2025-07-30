d438e055891d8fabd6fab06aa3435571f451254e
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-30 15:37:49 +0600

    task2 complite

---
+++ loops/loop_practice/even_odd.js
@@ -0,0 +1,15 @@
+// task 1 odd
+for(let i=61; i<=100;i++){
+    if(i%2===0){
+        document.write(i,"<br>")
+    }
+} 
+// task2 even 
+
+
+for(let i=78;i<=98;i++){
+    if(i%2===1){
+       document.write(i,'<br>')
+    }
+}
+
