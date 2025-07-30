d0b3bbbcf1f9f56229ca489ce9f8755da03a8739
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-30 15:51:51 +0600

    task 3 complite

---
+++ loops/loop_practice/sum_odd_nmbr.js
@@ -0,0 +1,20 @@
+//task1 
+/*
+let sum=0 
+
+for( let i=91; i<=129;i++){
+    if(i%2 !==0){
+        sum=sum+i
+    }
+} 
+document.write(sum,"<br>") */
+
+// task2 
+let sum=0
+
+for(let i=51;i<=85;i++){
+    if(i%2 !==1){
+        sum=sum+i
+    }
+} 
+document.write(sum)
