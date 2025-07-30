1eee7988a702867a17b0f2a30d322891537e49b4
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-30 11:35:09 +0600

    js foor loop while loop done

--- loops/while.js
+++ loops/while.js
@@ -1,4 +1,4 @@
-let num=1
+/*let num=1
 let sum=0 
 
 while(num <10 ){
@@ -9,4 +9,28 @@ while(num <10 ){
     document.write("sum is :",sum+"<br>")
     num=num+1
 
-}
+} */  
+/*
+  for(let i=0; i<10 ; i++){
+    document.write(i) 
+        if(i>=5){
+            break
+        }
+    
+  } */
+
+        /* for( let i=1;i<10;i++){
+            if(i%2===1){
+                continue
+                
+            }
+            document.write(i)
+       }   */
+
+        for(let i=1;i<20;i++){
+            if(i%2===1){
+                continue
+            }
+            document.write(i)
+        }
+
