9d4ce34620902ea8df2d5c25e9bc3ff2d0042e05
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-30 17:30:01 +0600

    while loop

---
+++ loops/while loop/comment1.js
@@ -0,0 +1,26 @@
+ /* let count=1
+ while(count <=60){
+    document.write(count,"I will invest at least 6 hrs every single day for next 60 days!<br>")
+    count++
+ } 
+    */ 
+  /* 
+
+ let number=61 
+ while(number<=100){
+   if( number%2 !==0){
+      document.write(number,"<br>")
+   } 
+   number++
+ } */
+   
+   let num=81
+   let sum=0
+   while(num<=100){
+      if(num % 2 ===0 ){ 
+         sum=sum+num
+      }
+      num++
+   } 
+   document.write(sum,'<br>')
+  
