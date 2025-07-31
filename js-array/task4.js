1c84d8485768952c158d1b9e6becb89d574bc707
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-31 17:29:47 +0600

    task4

---
+++ js-array/task4.js
@@ -0,0 +1,18 @@
+
+let name=['porag','fuad','badhon']
+ let age=20
+
+
+ if(Array.isArray(name)){
+    document.write("This is array")
+ } 
+ else{
+    document.write("this is not array")
+ } 
+
+ if(Array.isArray(age)){
+    document.write(" age is array")
+ } 
+ else{
+    document.write(" age  is not array")
+ }
