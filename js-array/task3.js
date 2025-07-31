9f83764be42ede264e336c9a03a573e44fee6559
fuadhasan199 <fuadhasan740266@gmail.com>
2025-07-31 17:08:03 +0600

    task3

---
+++ js-array/task3.js
@@ -0,0 +1,23 @@
+ 
+ /*let books=['bangla','English','math','python','JavaScript']
+
+ if(books.includes("science")){
+    document.write("Book is here")
+ } 
+ else{
+    document.write("book is not found")
+ } */
+
+    let abc=['porag','fuad','badhon','hasan']
+
+    if(abc.includes('dipu')){
+        document.write("name found")
+    } 
+    else{
+   document.write("This name not found")
+    }
+ 
+
+
+
+   
