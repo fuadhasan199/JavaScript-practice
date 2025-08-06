5f3d3df942c8e5cb305a9687d2628cb7c3806ba9
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-06 23:08:45 +0600

    more array 3

---
+++ more array-function/array3.js
@@ -0,0 +1,14 @@
+ 
+
+ const emp=[ 
+
+  { name:'fuad' , profesion:'Front end', salary:25000} , 
+  { name:'Porag',profesion:'Graphic deg',salary:18000}, 
+  
+  {name:'alex',profesion:'Digital Marketer',salary:15000}
+
+ ]  
+  
+ for (let doc of emp){
+    console.log( "Name :",doc.name,'Salary :',doc.salary);
+ }
