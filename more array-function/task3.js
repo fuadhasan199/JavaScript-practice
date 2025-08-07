85a43bd7b2ebaa1b47871812fc75c26712d2e78e
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-07 18:30:22 +0600

    ph task 3

---
+++ more array-function/task3.js
@@ -0,0 +1,16 @@
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'] 
+ /*  
+ var result=[] 
+
+ for ( var num of numbers){
+
+    result=result+num
+ } 
+ console.log(result);*/  
+
+ var result= " ";
+
+ for( let num of numbers){
+    result +=num
+ } 
+ console.log(result);
