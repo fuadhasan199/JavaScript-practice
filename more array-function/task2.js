d5344e1acee676da403197f5c9691dfd23d349a4
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-07 14:20:40 +0600

    ph task 2

---
+++ more array-function/task2.js
@@ -0,0 +1,15 @@
+
+const number=[12,98,5,41,23,76,46]
+ 
+
+const even_number=[] 
+
+for( const i of number){
+
+    if(i%2==0){
+        even_number.push(i)
+    }
+
+
+} 
+console.log(even_number);
