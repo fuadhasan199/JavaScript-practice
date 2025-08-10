352a28a16c7f56b2c150e8ec0f429429e7330c90
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-10 16:54:40 +0600

    problem 2

---
+++ problem solving/task2.js
@@ -0,0 +1,24 @@
+const number=[5,6,11,12,98, 5] 
+const find=5 
+let count=0 
+
+for(let i=0; i<number.length;i++){
+
+    if(number[i]===find){
+        count=count+1
+    }
+} 
+console.log(count); 
+
+const number_2= [5,6,11,12,98, 5]
+
+const find_2=25 
+let count_2=0 
+
+for(let i=0;i<number_2.length;i++){
+    if(number_2[i]===find_2){
+        count_2=count_2+1
+    }
+} 
+
+console.log(count_2);
