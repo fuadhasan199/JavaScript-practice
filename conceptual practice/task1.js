ec65497349c47915ce2cb656c222e36e32d985a6
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-12 22:09:53 +0600

    phone number

--- conceptual practice/task1.js
+++ conceptual practice/task1.js
@@ -1,21 +1,35 @@
 
-// function add(a,b){
-//     let result=a+b 
-//      return result
-// }  
-// function multiplay(a,b){
-//     let result=a*b 
-//     return result
+// // function add(a,b){
+// //     let result=a+b 
+// //      return result
+// // }  
+// // function multiplay(a,b){
+// //     let result=a*b 
+// //     return result
+// // } 
+
+// // let a=2
+// // let b=3 
+
+// // let final_result=add(a,b)+multiplay(a,b)
+// // console.log(final_result); 
+
+// function cashOut(money){
+//     let charge= money * (1.75/100) 
+//     return charge
 // } 
+// console.log(cashOut(100).toFixed(2)); 
 
-// let a=2
-// let b=3 
 
-// let final_result=add(a,b)+multiplay(a,b)
-// console.log(final_result); 
 
-function cashOut(money){
-    let charge= money * (1.75/100) 
-    return charge
+function validNumber(contact){
+
+if(contact.length==11 && contact.startsWith("01")==true && contact.includes("")==true){
+     return true 
+} else{
+    return false
+}
+    
+
 } 
-console.log(cashOut(100));
+console.log(validNumber("017877490952"));
