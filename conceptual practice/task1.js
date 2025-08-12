4505932b0d856cec731f8b5992e8401b6003ee9c
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-12 21:46:13 +0600

    conceptual 1

---
+++ conceptual practice/task1.js
@@ -0,0 +1,21 @@
+
+// function add(a,b){
+//     let result=a+b 
+//      return result
+// }  
+// function multiplay(a,b){
+//     let result=a*b 
+//     return result
+// } 
+
+// let a=2
+// let b=3 
+
+// let final_result=add(a,b)+multiplay(a,b)
+// console.log(final_result); 
+
+function cashOut(money){
+    let charge= money * (1.75/100) 
+    return charge
+} 
+console.log(cashOut(100));
