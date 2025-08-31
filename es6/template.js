27265635b16c4ef5770416a8723e4f581f2a6c83
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-31 10:50:39 +0600

    template function

---
+++ es6/template.js
@@ -0,0 +1,7 @@
+// const introduce=(name,age) => `HEllo , I am ${name} and ${age} years old`
+
+// console.log(introduce( 'Fuad Hasan',21));
+  
+const country=(a) => `This Country name is${a}`
+console.log("Bangladesh");
+  
