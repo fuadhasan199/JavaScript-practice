5524e896f3890ceffc13410acc7b1431d474716e
fuadhasan199 <fuadhasan740266@gmail.com>
2025-08-31 23:03:30 +0600

    foreach.js

---
+++ es6/foreach.js
@@ -0,0 +1,13 @@
+
+
+//  numbers.forEach((number,index,arr)=>{
+
+//     console.log('number :',number);
+//     console.log('index :',index);
+    
+//  })
+  const numbers = [10, 20, 30, 40]; 
+
+  numbers.forEach((num) => {
+    console.log(num/2);
+  })
